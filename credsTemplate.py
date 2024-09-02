5d033c75264ee00e97da0c8e696b7f466e35db70
Rohit <rohit.truesdale@gmail.com>
2024-09-02 17:35:15 -0500

    redirect info

--- credsTemplate.py
+++ credsTemplate.py
@@ -2,4 +2,4 @@
 
 CLIENT_ID = "insert_client_id_here"
 CLIENT_SECRET = "insert_client_secret_here"
-REDIRECT_URI = "insert_client_redirect_here"
+REDIRECT_URI = "insert_client_redirect_here" # http://localhost:8080/callback - Example 
