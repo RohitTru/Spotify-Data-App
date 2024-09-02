620f6a0fd6781921344669ba603938df643edb4e
Rohit <rohit.truesdale@gmail.com>
2024-09-02 01:06:07 -0500

    consolidation

--- sqliteDatabase.py
+++ sqliteDatabase.py
@@ -1,6 +1,10 @@
 import sqlite3
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from creds import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI 
 
 class SpotifyDatabase:
+    
     def __init__(self, filename):
         """Initialize the database connection."""
         self.databaseName = filename
@@ -52,8 +56,36 @@ class SpotifyDatabase:
         return data
 
 
+
+tracks = []
+def spotifyRetrieval(limit):
+    
+    scope = 'user-read-recently-played'
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope=scope))
+
+    results = sp.current_user_recently_played(limit)
+
+
+    for item in results['items']:
+        track = item['track']
+        played_at = item['played_at']
+        
+        track = (track['name'],track['artists'][0]['name'], played_at)
+        
+        tracks.append(track)
+
+    return tracks
+
+
+
+
+
 if __name__ == '__main__':
-    # Example usage
+ 
     db = SpotifyDatabase("spotifyData.db")
     db.create_table()
     db.insert_into_spotify_database('dangerous', '21 Savage', '2024-08-31 22:25:52.645000-04:00')
