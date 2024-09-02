dbad9147f9c5dc09aa0c1a4ac51d6f4cb43639fa
Rohit <rohit.truesdale@gmail.com>
2024-09-02 18:10:29 -0500

    deleted credstemplate.env

--- credsTemplate.py
+++
@@ -1,5 +0,0 @@
-# After replacing the values below, save and rename this file to  'creds.py'
-
-CLIENT_ID = "insert_client_id_here"
-CLIENT_SECRET = "insert_client_secret_here"
-REDIRECT_URI = "insert_client_redirect_here" # http://localhost:8080/callback - Example 
