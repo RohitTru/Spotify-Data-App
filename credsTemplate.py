970cd2514986029941ebb89fe2bdffc03a3caaa8
Rohit <rohit.truesdale@gmail.com>
2024-09-02 17:29:15 -0500

    removed real client_secret from template file

--- credsTemplate.py
+++ credsTemplate.py
@@ -1,5 +1,5 @@
 # After replacing the values below, save and rename this file to  'creds.py'
 
 CLIENT_ID = "insert_client_id_here"
-CLIENT_SECRET = "insert_client_secret_here 1c03da435d3740e4aa74f5985ffcd18b"
+CLIENT_SECRET = "insert_client_secret_here"
 REDIRECT_URI = "insert_client_redirect_here"
